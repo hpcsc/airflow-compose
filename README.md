ef5b9b24a52868d017220a6e846144b65054c13b
David Nguyen <hpcsc@users.noreply.github.com>
2021-01-25 22:21:12 +1100

    Add script to create Azure AD application

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ docker-compose project to create a local airflow deployment with Azure Active Di
 
 ## Start
 
-- Register an application in Azure AD
-- Add `upn` to the application `Token configuration`
+- Execute `./ad-app/create-ad-app.sh your-application`. This script registers an application with name `your-application` in Azure AD with:
+    - delegated permissions for `openid` and `profile`
+    - additional claim `upn`. By default Azure AD application is not returning this claim, which is required by airflow
+    - add application secret
+- Note down application id and password from output of script above
 - Create a file `oauth-secrets.env` file at the root folder with content:
 
 ```
-AZURE_APPLICATION_ID=client-id-from-azure-ad
-AZURE_SECRET=client-secret-from-azure-ad
+AZURE_APPLICATION_ID=application-id-from-step-1
+AZURE_SECRET=password-from-step-1
 AZURE_TENANT_ID=tenant-id-from-azure-ad
 ```
 
---
+++ ad-app/create-ad-app.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+APP_NAME=$1
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# Microsoft Graph resource app id: 00000003-0000-0000-c000-000000000000
+# openid scope id: 37f7f235-527c-4136-accd-4a02d197296e
+# profile scope id: 14dad69e-099b-42c9-810b-d002981feec1
+APP_CREATION_OUTPUT=$(az ad app create \
+    --display-name ${APP_NAME} \
+    --credential-description default \
+    --reply-urls http://localhost:8080/oauth-authorized/azure \
+    --required-resource-accesses @${SCRIPT_DIR}/resource-accesses.json \
+    --optional-claims @${SCRIPT_DIR}/optional-claims.json \
+    -o json)
+
+APP_ID=$(echo ${APP_CREATION_OUTPUT} | jq -r '.appId')
+
+echo "=== Application with id ${APP_ID} created"
+
+az ad app credential reset --credential-description default --id ${APP_ID} -o json
+echo "=== Client secret for app ${APP_ID} reset"
+
+# az ad app permission admin-consent --id ${APP_ID}
